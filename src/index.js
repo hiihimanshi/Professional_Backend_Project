15e7ef3b25935a63a78ba60346581208278faf8c
hiihimanshi <hiihimanshi@gmail.com>
2025-04-03 19:41:39 +0530

    connected to  MongoDB

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dotenv').config({path:"./env"})
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constant"
+
+import dotenv from "dotenv"
+import connectDB from "./db/function.js" 
+
+dotenv.config({
+    path:"./env"
+})
+connectDB()
+
+// first approach of connecting db
+// import express from "express"
+// const app = express()
+// // (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         // listener
+//         app.on("error", (error)=>{
+//             console.error("Error:",error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.error("Error: ", error)
+//         throw error
+//     }
+// })()
